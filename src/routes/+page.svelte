b80ab4531f1569aacb4b1093a09fd8a4313a669d
Maxwell <max@dirty-martini.com>
2023-02-07 10:37:19 +0000

    p5 added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	export let data;
+	import Logo from '$lib/p5/logo.svelte';
 	const document = data.document.data;
 
 	// form prismic
@@ -11,6 +12,7 @@
 
 <h1>{page.title}</h1>
 <h3>{page.subtitle}</h3>
+<Logo />
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
